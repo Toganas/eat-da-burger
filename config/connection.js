2f6712acd4add5b7a7d3eab5f5adbf01ada5745d
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-18 22:20:23 -0500

    exporting the connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
+// require mysql
 var mysql = require('mysql');
+// set up connection info
 var conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    // grab password from .env
     password: process.env.password,
     database: 'burgers_db'
 });
 
-conn.connect();
+// connect to database
+conn.connect((err)=>{
+    if(err) {
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadID);
+});
 
-
-
-conn.end();
+// export connection
+module.exports = conn;
