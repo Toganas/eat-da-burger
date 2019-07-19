f205048326a37021c29c401b3d24d3b990a26109
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-18 23:12:33 -0500

    created burger.js with orm commands to manipulate the data.  node still says it's working.  don't really have a way to test it though :(.  module.exports = burger put at the end.

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+// requiring orm
+const orm = require("../config/orm.js");
+
+let burger = {
+    // selecting all
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res){
+            cb(res);
+        });
+    },
+    // Adding a burger to the database
+    insertOne: (col, burger, cb) => {
+        orm.create("burgers", col, burger, (res) => {
+            cb(res);
+        })
+    },
+    // updating a burgers devour column
+    updateOne: (col, col2, id, cb) => {
+        orm.update("burgers", col, col2, id, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
