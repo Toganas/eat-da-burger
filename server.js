258dbe26e33e46cc9149241004396ec1da51ca5e
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-19 19:55:47 -0500

    added bootstrap to main.handlebars, begain creation of index.handlebars (setting a header in a jumbotron and creating divs to house to burgers.  Built the server.js (that I should've done earlier).  Built everything in that one

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+// require dependencies
 const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars")
+// acquire the port
+let PORT = process.env.PORT || 3000;
+
+// setting static content as the public directory.
+app.use(express.static("public"));
+
+// parse into json
+app.use(express.urlencoded ({ extended: true }))
+app.use(express.json());
+
+// set up handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// give server access to routes
+const routes = require("./controllers/burgers_controller.js")
+app.use(routes);
+
+// listen to the server response so we know we're up and running
+
+console.log("Server listening on http://localhost:" + PORT);
