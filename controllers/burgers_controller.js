ce41e8a15c3b1ecc821b8e436b1f129fedfd6a4a
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-23 00:43:25 -0500

    changed hbsObject to burger_data so it would flow better

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,27 +10,27 @@ router.get("/", (req, res) => {
     burger.selectAll((data, err) => {
         // handlebars object
         if (err) throw err;
-        let hbsObject = {
+        let burger_data = {
             burgers: data
         };
-        console.log(hbsObject);
+        console.log(burger_data);
         // render on page using the index handlebars page
-        res.render("index", hbsObject);
+        res.render("index", burger_data);
     });
 });
 // // creating an api from the info posted on the page
 router.post("api/burgers", (req, res) => {
     // creating the burger
     burger.insertOne(req.body.burger_name, (result, err) => {
-            // sending back the result as a json
-            if (err) throw err;
-            res.redirect("/")
-        });
+        // sending back the result as a json
+        if (err) throw err;
+        res.redirect("/")
+    });
 });
 
 // updating the devour
 router.put("/api/burger/:id", (req, res) => {
-       burger.updateOne(req.params.id, (result, err) => {
+    burger.updateOne(req.params.id, (result, err) => {
         if (err) throw err;
         if (result.changedRows == 0) {
             // no rows were changed, which means the ID isn't there, make it a 404
