0e9b118cfbdfb1ff965c06b333f7fe9d7119ed22
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-23 00:33:54 -0500

    changed the orm. calls to make the correct functions.  changed updateOne so that it wouldn't need as many values as that was making it much more complicated than I needed it to be

--- models/burger.js
+++ models/burger.js
@@ -4,21 +4,22 @@ const orm = require("../config/orm.js");
 let burger = {
     // selecting all
     selectAll: (cb) => {
-        orm.all("burgers", (res)=>{
+        orm.selectAll("burgers", (res) => {
             cb(res);
         });
     },
     // Adding a burger to the database
     insertOne: (burger, cb) => {
-        orm.create("burgers", "burger_name", burger, (res) => {
+        orm.insertOne("burgers", "burger_name", burger, (res) => {
             cb(res);
         })
     },
     // updating a burgers devour column
-    updateOne: (val1, val2, cb) => {
-        orm.update("burgers", "devoured", val1, val2, (res) => {
-            cb(res);
-        });
+    updateOne: (id, cb) => {
+        let condition = "id=" + id;
+        orm.updateOne("burgers", {
+            devoured: true
+        }, condition, db);
     }
 };
 
