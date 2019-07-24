e9b0874473849ed9c8cfd81626b4dd709aa8266c
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-24 16:14:17 -0500

    removed the port from the connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ if (process.env.JAWSDB_URL){
 } else {
     conn = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
     user: 'root',
     // grab password from .env
     password: process.env.password,
