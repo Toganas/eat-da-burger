7cf1b04ed3d7cfd721b9122a5291a4a3b4b5ea92
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-24 16:24:14 -0500

    changed the schema, put in commas, moved PRIMARY KEY(id) to the end

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY;
+    id INT AUTO_INCREMENT NOT NULL;
     burger_name VARCHAR(100) NOT NULL;
     devoured BOOL DEFAULT false
+    PRIMARY KEY(id)
 )
