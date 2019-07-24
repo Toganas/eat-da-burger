d8a6a49ed1dc8dcaba418801a1eb48a856ce1d0f
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-23 20:39:12 -0500

    changed from an arrow function to function(event) and it started working.  uncommented out the ajax call, it now works

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(() => {
 
-    $(".eatMe").on("submit", (event) => {
+    $(".eatMe").on("submit", function(event) {
         event.preventDefault();
         // capturing the value of burger_id
         let burger_id = $(this).children(".burger_id").val();
         console.log(burger_id);
+
         // ajax call to put
-        // $.ajax({
-        //     url: "/api/burger/" + burger_id,
-        //     method: "PUT"
-        // }).then((result) => {
-        //     location.reload();
-        // })
+        $.ajax({
+            method: "PUT",
+            url: "/burgers/" + burger_id            
+        }).then(function() {
+            location.reload();
+        })
     });
 });
